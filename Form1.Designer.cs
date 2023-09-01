f007d95e50caad4f6f0c6c3b66585b8d51442f22
Daniel <alcadaniel1112@gmail.com>
2023-09-01 17:29:31 -0300

    primera-Subida..

---
+++ Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace AplicacionWindows_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.lbl4 = new System.Windows.Forms.Label();
+            this.lbl5 = new System.Windows.Forms.Label();
+            this.tbx1 = new System.Windows.Forms.TextBox();
+            this.tbx4 = new System.Windows.Forms.TextBox();
+            this.tbx3 = new System.Windows.Forms.TextBox();
+            this.tbx2 = new System.Windows.Forms.TextBox();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.tbxResultado = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Carlito", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.Location = new System.Drawing.Point(21, 12);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(53, 14);
+            this.lbl1.TabIndex = 6;
+            this.lbl1.Text = "Nombre: ";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Font = new System.Drawing.Font("Carlito", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl2.Location = new System.Drawing.Point(21, 48);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(50, 14);
+            this.lbl2.TabIndex = 7;
+            this.lbl2.Text = "Apellido:";
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Font = new System.Drawing.Font("Carlito", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl3.Location = new System.Drawing.Point(21, 92);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(34, 14);
+            this.lbl3.TabIndex = 8;
+            this.lbl3.Text = "Edad:";
+            // 
+            // lbl4
+            // 
+            this.lbl4.AutoSize = true;
+            this.lbl4.Font = new System.Drawing.Font("Carlito", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl4.Location = new System.Drawing.Point(21, 129);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(59, 14);
+            this.lbl4.TabIndex = 9;
+            this.lbl4.Text = "Dirección: ";
+            // 
+            // lbl5
+            // 
+            this.lbl5.AutoSize = true;
+            this.lbl5.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl5.Location = new System.Drawing.Point(21, 168);
+            this.lbl5.Name = "lbl5";
+            this.lbl5.Size = new System.Drawing.Size(88, 14);
+            this.lbl5.TabIndex = 10;
+            this.lbl5.Text = "Resultados: ";
+            // 
+            // tbx1
+            // 
+            this.tbx1.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.tbx1.Location = new System.Drawing.Point(78, 9);
+            this.tbx1.MaxLength = 10;
+            this.tbx1.Name = "tbx1";
+            this.tbx1.Size = new System.Drawing.Size(392, 20);
+            this.tbx1.TabIndex = 0;
+            this.tbx1.Validated += new System.EventHandler(this.tbx1_Validated);
+            // 
+            // tbx4
+            // 
+            this.tbx4.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.tbx4.Location = new System.Drawing.Point(78, 126);
+            this.tbx4.MaxLength = 18;
+            this.tbx4.Name = "tbx4";
+            this.tbx4.Size = new System.Drawing.Size(392, 20);
+            this.tbx4.TabIndex = 3;
+            this.tbx4.Validated += new System.EventHandler(this.tbx4_Validated);
+            // 
+            // tbx3
+            // 
+            this.tbx3.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.tbx3.Location = new System.Drawing.Point(78, 89);
+            this.tbx3.MaxLength = 3;
+            this.tbx3.Name = "tbx3";
+            this.tbx3.Size = new System.Drawing.Size(61, 20);
+            this.tbx3.TabIndex = 2;
+            this.tbx3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbx3_KeyPress);
+            this.tbx3.Validated += new System.EventHandler(this.tbx3_Validated);
+            // 
+            // tbx2
+            // 
+            this.tbx2.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.tbx2.Location = new System.Drawing.Point(78, 45);
+            this.tbx2.MaxLength = 10;
+            this.tbx2.Name = "tbx2";
+            this.tbx2.Size = new System.Drawing.Size(392, 20);
+            this.tbx2.TabIndex = 1;
+            this.tbx2.Validated += new System.EventHandler(this.tbx2_Validated);
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(112, 401);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(75, 23);
+            this.btn1.TabIndex = 4;
+            this.btn1.Text = "Aceptar ";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(326, 401);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(75, 23);
+            this.btn2.TabIndex = 5;
+            this.btn2.Text = "Cancelar ";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // tbxResultado
+            // 
+            this.tbxResultado.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.tbxResultado.Enabled = false;
+            this.tbxResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxResultado.Location = new System.Drawing.Point(78, 200);
+            this.tbxResultado.Multiline = true;
+            this.tbxResultado.Name = "tbxResultado";
+            this.tbxResultado.Size = new System.Drawing.Size(392, 172);
+            this.tbxResultado.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CadetBlue;
+            this.ClientSize = new System.Drawing.Size(528, 450);
+            this.Controls.Add(this.tbxResultado);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.tbx2);
+            this.Controls.Add(this.tbx3);
+            this.Controls.Add(this.tbx4);
+            this.Controls.Add(this.tbx1);
+            this.Controls.Add(this.lbl5);
+            this.Controls.Add(this.lbl4);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.MaximumSize = new System.Drawing.Size(544, 489);
+            this.MinimumSize = new System.Drawing.Size(544, 489);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Datos Personales ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Label lbl4;
+        private System.Windows.Forms.Label lbl5;
+        private System.Windows.Forms.TextBox tbx1;
+        private System.Windows.Forms.TextBox tbx4;
+        private System.Windows.Forms.TextBox tbx3;
+        private System.Windows.Forms.TextBox tbx2;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.TextBox tbxResultado;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AplicacionWindows_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tbx1_Validated(object sender, EventArgs e)  
+        {
+            if (tbx1.Text == string.Empty)
+            {
+                tbx1.BackColor = Color.Red;
+                MessageBox.Show("CAMPO OBLIGATORIO.");
+            }
+            else if (tbx1.Text.Length >= 4)
+            {
+                tbx1.BackColor = SystemColors.Window;
+            }
+            else 
+                MessageBox.Show("Debe contener al menos 4 caracteres. "); 
+        }
+
+        private void tbx2_Validated(object sender, EventArgs e)
+        {
+            if (tbx2.Text == string.Empty)
+            {
+                tbx2.BackColor = Color.Red;
+                MessageBox.Show("CAMPO OBLIGATORIO.");
+            }
+            else if (tbx2.Text.Length >= 4)
+            {
+                tbx2.BackColor = SystemColors.Window;
+            }
+            else
+                MessageBox.Show("Debe contener al menos 4 caracteres. ");
+        }
+
+        private void tbx3_Validated(object sender, EventArgs e)
+        {
+            if (tbx3.Text == string.Empty)
+            {
+                tbx3.BackColor = Color.Red;
+                MessageBox.Show("CAMPO OBLIGATORIO.");
+            }
+            else
+                tbx3.BackColor = SystemColors.Window;
+        }
+
+        //Para ingresar solo nros en elTextBoxEdad
+        private void tbx3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true; 
+            }
+        }
+
+        private void tbx4_Validated(object sender, EventArgs e)
+        {
+            if (tbx4.Text == string.Empty)
+            {
+                tbx4.BackColor = Color.Red;
+                MessageBox.Show("CAMPO OBLIGATORIO.");
+            }
+            else if (tbx4.Text.Length >= 4)
+            {
+                tbx4.BackColor = SystemColors.Window;
+            }
+            else
+                MessageBox.Show("Debe contener al menos 4 caracteres. ");
+        }
+
+
+        //Agregar para el ListWiew. 
+        private int conAceptar = 1; 
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            string textoNombre = tbx1.Text;
+            string textoApellido = tbx2.Text;
+            string textoEdad = tbx3.Text;
+            string textoDireccion = tbx4.Text;
+            
+            if (conAceptar == 1 && !string.IsNullOrWhiteSpace(textoNombre) && !string.IsNullOrWhiteSpace(textoApellido) && !string.IsNullOrWhiteSpace(textoEdad) && !string.IsNullOrWhiteSpace(textoDireccion))
+            {
+                string formatoNombre = new string('x', Math.Max(15, textoNombre.Length)); 
+                tbxResultado.Text += formatoNombre + Environment.NewLine; 
+                string formatoApellido = new string('x', Math.Max(15, textoApellido.Length));
+                tbxResultado.Text += formatoApellido + Environment.NewLine;
+                string formatoEdad = new string('x', Math.Max(3, textoEdad.Length));
+                tbxResultado.Text += formatoEdad + Environment.NewLine;
+                string formatoDireccion = new string('x', Math.Max(20, textoDireccion.Length));
+                tbxResultado.Text += formatoDireccion + Environment.NewLine; 
+                conAceptar++; 
+            }
+        }
+
+        //Cerrar con el btn2 = "Cancelar"
+        private bool btn2Clicked = false;
+        private bool cerrando = false;
+        private int contador = 1;
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            if (!btn2Clicked)
+            {
+                MessageBox.Show("Gracias por su tiempo...");
+                cerrando = true;
+                this.Close();
+                btn2Clicked = true;
+            }
+        }
+
+    }
+}
